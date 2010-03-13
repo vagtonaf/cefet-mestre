ae5d2fb1b50674fe3481352fe990234abda64de0
vagtonaf <vagtonaf@75509a64-2adf-11df-9f82-5b465c4f9b88>
2010-03-13 20:04:36 +0000

    Atualização disciplina plano_de_prova, prova

--- trunk/Mestre/controllers/mcrud.py
+++ trunk/Mestre/controllers/mcrud.py
@@ -12,6 +12,7 @@ def cadlist():
             redirect(URL(r=request,f='../default/error'))
         elif form.errors: response.flash='Erro em seu formulário'
         row_professor=db(db.professor.usuario==auth.user.id).select(db.professor.ALL)
+	# aqui no if somente as tabelas que tem um select especial valeu
         if tabela=='aluno':
             if row_professor:
                 registros=db(db.aluno.matricula>0).select(
@@ -47,21 +48,47 @@ def cadlist():
                     db.auth_user.ALL,
                     left=db.auth_user.on(db.alocacao.aluno==db.auth_user.id),distinct=True
                 )
+		return dict(registros=registros,form=form, tabela=tabela, list_turmas=list_turmas, ist_aluno=ist_aluno)
             else:
                 redirect(URL(r=request,f='../default/erro_acesso'))
             response.flash='Cadastra e Lista ' + tabela
-            return dict(registros=registros, form=form, tabela=tabela, list_turmas=list_turmas, list_alunos=list_alunos)
-        elif: tabela=='disciplina':
+            return dict(registros=registros, form=form, list_turmas=list_turmas, list_alunos=list_alunos, tabela=tabela)
+        elif tabela=='disciplina':
             if row_professor:
                 registros= disciplinas=db(db.disciplina.id>0).select(
                     db.disciplina.ALL,
                     db.curso.ALL,
                     left=db.curso.on(db.disciplina.curso==db.curso.id)
-                )      
-        else:
-            registros=db(db[tabela].id>0).select(db[tabela].ALL)
+                )  
+	elif tabela=='plano_de_prova':
+            if row_professor:
+		form2=crud.create(db.item_plano_de_prova)
+		professores=db(db.professor.id>0).select(db.professor.ALL)
+		registros=db(db.plano_de_prova.id>0).select(
+			db.plano_de_prova.ALL,
+			db.item_plano_de_prova.ALL,
+			left=db.item_plano_de_prova.on(db.item_plano_de_prova.plano_de_prova==db.plano_de_prova.id)
+		)
+		registros.db(db.plano_de_prova.taxionomia>0).select(db.taxionomia.ALL,left=db.plano_de_prova.on(db.plano_de_prova.taxionomia==db.taxionomia.id))
+		response.flash='Lista Plano de Prova'
+		return dict(registros=registros, professores=professores, form=form, form2=form2, tabela=tabela)
+        elif tabela=='prova':
+		registros=db(db.prova.id>0).select(db.prova.ALL)
+		turmas=db(db.turma.id>0).select(db.turma.ALL)
+		if 'auth' in globals():
+			if auth.is_logged_in(): 
+				planoprovas=db(db.plano_de_prova.id>0).select(db.plano_de_prova.ALL,db.professor.ALL,left=db.professor.on( (db.professor.id==db.plano_de_prova.professor )& (db.professor.usuario==auth.user.id)))
+				response.flash='Lista Prova'
+				return dict(registros=registros, turmas=turmas, planoprovas=planoprovas, form=form, tabela=tabela)
+                        else:
+				redirect(URL(r=request,f='../default/erro_acesso'))
+                else:
+			redirect(URL(r=request,f='../default/erro_acesso'))
+	else:
+		# esse é um select genérico, serve para a maioria das tabelas
+		registros=db(db[tabela].id>0).select(db[tabela].ALL)
     except KeyError, NameError:
-        tabela='error'
+	tabela='error'
         response.flash='Tabela Inexistente!'
         form=''
         return dict(form=form, registros='Não posso listar esta tabela!', tabela=tabela)   
