d23eb4a0515fd505d13756667478923ba43111b0
vagtonaf <vagtonaf@75509a64-2adf-11df-9f82-5b465c4f9b88>
2010-04-26 00:33:00 +0000

    resultado_prova

--- trunk/Mestre/controllers/relatorios.py
+++ trunk/Mestre/controllers/relatorios.py
@@ -23,3 +23,35 @@ def rel_aluno_nota():
     
 def graf_bar():
     return dict()
+
+@auth.requires_login()
+def resultado_prova():
+	idAluno=request.args(0)
+	usuario = db(db.aluno.id==idAluno).select(db.aluno.usuario)
+	if usuario:
+		for u in usuario:
+			Aluno=u.usuario.first_name + ' ' + u.usuario.last_name
+	else:
+		response.flash = 'Nao e um aluno cadastrado!'
+		msg = FORM(TABLE(TR('Nao e aluno!')))
+		return dict(msg=msg)
+	total=0
+	provarealizada = db(db.prova_gerada.aluno==idAluno).select(db.prova_gerada.ALL, db.item_prova_gerada.ALL,
+		left=db.item_prova_gerada.on(db.item_prova_gerada.prova_gerada==db.prova_gerada.id))
+	for ipr in provarealizada:
+		gerada = ipr.item_prova_gerada.prova_gerada.gerada
+		Prova1=ipr.item_prova_gerada.prova_gerada.prova.referencia
+		Data1 = ipr.item_prova_gerada.prova_gerada.prova.data_aplicacao
+		Data2 = ipr.item_prova_gerada.prova_gerada.data
+		PlanodeProva1=ipr.item_prova_gerada.prova_gerada.prova.plano_de_prova.referencia
+		alternativa=db(db.alternativa.questao==ipr.item_prova_gerada.questao and db.alternativa.id==ipr.item_prova_gerada.alternativa_escolhida).select(db.questao.ALL, db.alternativa.ALL, 
+			left=db.alternativa.on(db.alternativa.questao==db.questao.id))
+		for ssa in alternativa:
+			itemplanoprova=db(db.item_plano_de_prova.plano_de_prova==ipr.prova_gerada.prova.plano_de_prova).select(db.item_plano_de_prova.ALL)
+			for resp in itemplanoprova:
+				if resp.topico==ssa.questao.topico:
+					if resp.dificuldade==ssa.questao.dificuldade:
+						if resp.taxionomia==ssa.questao.taxionomia:
+							if ssa.alternativa.correta==True:
+								total = total + resp.valor
+	return dict(aluno=Aluno, prova=Prova1, Plano_de_Prova=PlanodeProva1, gerada=gerada, Data_Final=str(Data2), Data_Aplicacao=str(Data1), Resultado_Final=str(total),provarealizada=provarealizada)
