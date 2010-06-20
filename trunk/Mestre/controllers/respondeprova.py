e8e407deb6a9249dee7b2a3d9407a6550a51339c
vagtonaf <vagtonaf@75509a64-2adf-11df-9f82-5b465c4f9b88>
2010-06-20 20:46:54 +0000

    atualizações diversas

--- trunk/Mestre/controllers/respondeprova.py
+++ trunk/Mestre/controllers/respondeprova.py
@@ -94,10 +94,13 @@ def responder_prova():
                     realizar_prova = FORM(TABLE(TR('Aluno não possui email cadastrado!')))
                     return dict(realizar_prova=realizar_prova, row_prova=row_prova, raluno=row_aluno, rprova=row_prova, tabela='solicitacao')
                 #testa se o aluno está alocado a uma turma   
-                row_alocacao = db(db.alocacao.aluno==idAluno).select(db.alocacao.turma)
+                row_alocacao = db((db.alocacao.aluno==idAluno)&(db.prova.data_aplicacao!=None)).select(db.alocacao.turma,
+                                                                     db.prova.ALL,
+                                                                     left=db.prova.on(db.alocacao.turma==db.prova.turma)
+                                                                    )
                 if row_alocacao:
                     for alo in row_alocacao:
-                        idTurma=alo.turma
+                        idTurma=alo.prova.turma
                         row_turma = db(db.turma.id==idTurma).select(db.turma.nome)
                         for tu in row_turma:
                             nome_turma=tu.nome
--- trunk/Mestre/controllers/util.py
+++ trunk/Mestre/controllers/util.py
@@ -1,4 +1,4 @@
 # coding: utf8
 # try something like
-def construcao(): return dict(message="Site em Construção")
+def construcao(): return dict(message="Site em ConstruÃ§Ã£o")
 def creditos(): return dict(message="Creditos")
